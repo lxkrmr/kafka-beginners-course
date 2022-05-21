d75088318e8a737a620e53e87f6d96736afb873f
Alex <lxkrmr@icloud.com>
2022-05-21 14:17:17 +0200

    Added Consumer Demo

---
+++ kafka-basics/src/main/java/demos/kafka/ConsumerDemo.java
@@ -0,0 +1,58 @@
+package demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer");
+
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "my-second-application";
+        String topic = "demo_java";
+
+        // creating consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe consumer to our topics
+        consumer.subscribe(Collections.singleton(topic));
+
+        // poll for new data
+        while (true) {
+            log.info("Polling");
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info("""
+                                 Key: %s
+                                 Value: %s
+                                 Partition: %s
+                                 Offset: %s
+                                 """.formatted(record.key(),
+                                               record.value(),
+                                               record.partition(),
+                                               record.offset()));
+            }
+        }
+    }
+}
