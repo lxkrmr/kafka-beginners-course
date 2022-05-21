94c4276aff7161d54836ba21e737448484d41311
Alex <lxkrmr@icloud.com>
2022-05-21 13:57:19 +0200

    Added producer using a key to ensure that the record with the same id goes to the same partition

--- kafka-basics/src/main/java/demos/kafka/ProducerDemoKeys.java
+++ kafka-basics/src/main/java/demos/kafka/ProducerDemoKeys.java
@@ -1,15 +1,17 @@
 package demos.kafka;
 
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemoWithCallback {
+public class ProducerDemoKeys {
 
-    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
 
     public static void main(String[] args) {
         log.info("I am a Kafka Producer");
@@ -25,8 +27,13 @@ public class ProducerDemoWithCallback {
 
         for (int i = 0; i < 10; i++) {
             // create a producer record
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java",
-                                                                                 "hello world " + i);
+            String topic = "demo_java";
+            String value = "hello world " + i;
+            String key = "id_" + i;
+
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic,
+                                                                                 key,
+                                                                                 value);
 
             // send data - asynchronous
             producer.send(producerRecord, (metadata, e) -> {
@@ -36,11 +43,13 @@ public class ProducerDemoWithCallback {
                     log.info(""" 
                                      Received new metadata
                                      Topic: %s
+                                     Key: %s
                                      Partition: %s
                                      Offset: %s
                                      Timestamp: %s
                                      """.formatted(
                             metadata.topic(),
+                            producerRecord.key(),
                             metadata.partition(),
                             metadata.offset(),
                             metadata.timestamp()
@@ -50,12 +59,6 @@ public class ProducerDemoWithCallback {
                     log.error("Error while producing", e);
                 }
             });
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
 
         // flush and close the Producer - synchronous
